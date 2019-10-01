78c642cc5aba62c88bdd6266b44a61358607361e
NaeemCho <naimul.chowdhury15@ncf.edu>
2019-10-01 16:05:41 -0400

    Initialized iris_learn, a machine learning model that can learn from the measurements of irises whose species is known, so that we can predict the species for a new iris.

---
+++ iris_learn.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from iPython.display import display
+
+from sklearn.datasets import load_iris
+
+iris_dataset = load_iris()
